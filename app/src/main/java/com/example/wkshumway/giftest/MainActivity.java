ba614798409f9afa5dba4bd18477f7ca8d9a5bb4
wkshumway <wkshumway@gmail.com>
2016-03-18 10:18:21 -0600

    moretesting

--- app/src/main/java/com/example/wkshumway/giftest/MainActivity.java
+++ app/src/main/java/com/example/wkshumway/giftest/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
-import com.facebook.drawee.interfaces.DraweeController;
 import com.facebook.drawee.view.SimpleDraweeView;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,15 +17,15 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        Uri uri = Uri.parse("file:///Users/johnmeyer/Desktop/GifTest/app/src/main/res/drawable/whileloop.gif");
+        loadGif();
+
+    }
+
+    protected void loadGif() {
+
+        Uri uri = Uri.parse("file:///Users/wkshumway/StudioProjects/Hello_World/GifTest/app/src/main/res/drawable/whileloop.gif");
         SimpleDraweeView draweeView = (SimpleDraweeView) findViewById(R.id.my_image_view);
 
-        DraweeController controller = Fresco.newDraweeControllerBuilder()
-                .setUri(uri)
-                .setAutoPlayAnimations(true)
-                .build();
-
-        draweeView.setController(controller);
-
+        draweeView.setImageURI(uri);
     }
 }
